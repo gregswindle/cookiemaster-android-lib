370d9c6b48e14806395287446f1036e86927dac8
Greg Swindle <greg@swindle.net>
2017-04-15 10:17:27 -0500

    feat(code-coverage): add Codecov qa gate (#4)
    * chore(scm): coveralls
    
    * Apply coveralls plugin
    * Configure SonarQube gateway
    
    #1
    
    * chore(scm): codecov qa gate
    
    #2
    
    * chore(scm): codecov
    
    * chore(scm): codecov
    
    * chore(scm): add codecov checks to PRs
    
    * docs(README): fix badge links
    
    #2
    
    * chore(qa): use sonar-scanner
    
    #2
    
    * chore(qa): fix path
    
    #2
    
    * chore(scm): execute org.sonar.plugins.github.PullRequestProjectBuilder
    
    #2
    
    * chore(scm): add call to sonarScanOnPullRequest
    
    #2

---
+++ .travisBuild.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function sonarScanOnPullRequest {
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    sonar-scanner -Dsonar.analysis.mode=preview \
+                  -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+                  -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+                  -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN \
+                  -Dsonar.host.url=https://sonarqube.com \
+                  -Dsonar.login=$SONAR_TOKEN
+  fi
+}
+
+sonarScanOnPullRequest
